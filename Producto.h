b2f2b6afe77c4085506fa799fd3e78fa160779cd
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-09-18 20:04:24 -0600

    Producto.h

--- Producto.h
+++ Producto.h
@@ -1,1 +1,42 @@
+// Mariana Hernández Díaz
+//A01711207 
+
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+#include <string>
+#include <iostream>
+
+class Producto {
+public:
+    Producto() : nombre(""), categoria(""), precio(0.0), cantidad(0) {}
+
+    Producto(const std::string& nombre, const std::string& categoria, double precio, int cantidad)
+        : nombre(nombre), categoria(categoria), precio(precio), cantidad(cantidad) {}
+
+   
+    std::string getNombre() const {
+        return nombre;
+    }
+
+    int getCantidad() const {
+        return cantidad;
+    }
+
+   
+    void mostrarProducto() const {
+        std::cout << "Nombre: " << nombre << std::endl;
+        std::cout << "Categoría: " << categoria << std::endl;
+        std::cout << "Precio: $" << precio << std::endl;
+        std::cout << "Cantidad: " << cantidad << std::endl;
+    }
+
+private:
+    std::string nombre;
+    std::string categoria;
+    double precio;
+    int cantidad;
+};
+
+#endif // PRODUCTO_H
 
