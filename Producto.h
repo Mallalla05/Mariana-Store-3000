41b0d971f0824250cd661134a0f79f238a2a83ce
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-11-23 00:30:06 -0600

    Producto.h

--- Producto.h
+++ Producto.h
@@ -1,51 +1,48 @@
+// Productos.h
 // Mariana Hernández Díaz
 // A01711207
 
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
 
+// Se define la clase que presenta el producto.
 class Producto {
 public:
+// Constructor por defecto 0(1).
+  Producto() : nombre(""), categoria(""), precio(0.0), cantidad(0) {}
 
-    Producto() : nombre(""), categoria(""), precio(0.0), cantidad(0) {}
+// Constructor que inicia el producto con valores especificos 0(1).
+  Producto(const string &nombre, const string &categoria, double precio,
+           int cantidad)
+      : nombre(nombre), categoria(categoria), precio(precio),
+        cantidad(cantidad) {}
 
-    
-    Producto(const string& nombre, const string& categoria, double precio, int cantidad)
-        : nombre(nombre), categoria(categoria), precio(precio), cantidad(cantidad) {}
+// Se define el metodo para tener los atributos del producto.
+  string getNombre() const { return nombre; }
 
-    
-    string getNombre() const {
-        return nombre;
-    }
+  string getCategoria() const { return categoria; }
 
-    string getCategoria() const {
-        return categoria;
-    }
+  double getPrecio() const { return precio; }
 
-    double getPrecio() const {
-        return precio;
-    }
-
-    int getCantidad() const {
-        return cantidad;
-    }
-
-    void mostrarProducto() const {
-        cout << "Nombre: " << nombre << endl;
-        cout << "Categoría: " << categoria << endl;
-        cout << "Precio: $" << precio << endl;
-        cout << "Cantidad: " << cantidad << endl;
-    }
+  int getCantidad() const { return cantidad; }
+// Enseña los atributos del producto.
+  void mostrarProducto() const {
+    cout << "Nombre: " << nombre << endl;
+    cout << "Categoría: " << categoria << endl;
+    cout << "Precio: $" << precio << endl;
+    cout << "Cantidad: " << cantidad << endl;
+  }
 
 private:
-    string nombre;
-    string categoria;
-    double precio;
-    int cantidad;
+// Son los atributos del producto
+  string nombre;
+  string categoria;
+  double precio;
+  int cantidad;
 };
 
 #endif // PRODUCTO_H
